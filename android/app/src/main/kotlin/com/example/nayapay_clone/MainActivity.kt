61a47c491bf1e7a126ae0e5f9bb22f353fb351e1
mamhafil <mamhafil7864@gmail.com>
2023-09-16 22:10:39 +0530

    gooooli

--- android/app/src/main/kotlin/com/example/nayapay_clone/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nayapay_clone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/nayapayclone/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nayapayclone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
